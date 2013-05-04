333d36a2100f6c9bd28296d1ce701b3f368f1fb0
Aaron Neyer <aaronneyer@gmail.com>
2013-05-04 11:26:20 -0400

    Updated diagrams

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,9 +2,8 @@ class Player < ActiveRecord::Base
   belongs_to :current_team, class_name: "Team"
   has_many :players_teams
   has_many :players_games
-  has_and_belongs_to_many :teams
-  has_and_belongs_to_many :games
-  has_many :plays
+  has_many :teams, through: :players_teams
+  has_many :games, through: :players_games
   has_many :players_years
 
   scope :years_stats, -> {joins(:players_years => :stats)}
